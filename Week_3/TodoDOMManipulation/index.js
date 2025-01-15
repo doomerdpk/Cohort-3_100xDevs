8556c4820de3750826c58b1dd9af572620256c00
doomerdpk <doomerdpk@gmail.com>
2025-01-15 19:48:58 +0530

    Added Edit and Mark as Done Functionality

--- Week_3/TodoDOMManipulation/index.js
+++ Week_3/TodoDOMManipulation/index.js
@@ -5,6 +5,24 @@ function deleteTodo(index) {
   el.parentNode.removeChild(el);
 }
 
+function MarkDone(index) {
+  const el = document.getElementById("todo-" + index);
+  el.style.textDecoration = "line-through";
+}
+
+function EditTodo(index) {
+  const el = document.getElementById("todo-" + index);
+  const h3El = el.querySelector("h3");
+  h3El.contentEditable = true;
+  h3El.focus();
+}
+
+function SaveTodo(index) {
+  const el = document.getElementById("todo-" + index);
+  const h3El = el.querySelector("h3");
+  h3El.contentEditable = false;
+}
+
 function addTodo() {
   const todo = document.querySelector("input").value;
 
@@ -19,9 +37,15 @@ function addTodo() {
   newTodo.innerHTML =
     "<h3>" +
     todo +
-    "</h3><button class='deleteTodo' onclick='deleteTodo(" +
+    "</h3><div><button class='deleteTodo' onclick='deleteTodo(" +
     todoCtr +
-    ")'>Delete</button>";
+    ")'>Delete</button><button class='markTodo' onclick='MarkDone(" +
+    todoCtr +
+    ")'>Mark as Done</button><button class='editTodo' onclick='EditTodo(" +
+    todoCtr +
+    ")'>Edit Todo</button><button class='saveTodo' onclick='SaveTodo(" +
+    todoCtr +
+    ")'>Save Todo</button></div>";
 
   const parentEl = document.getElementById("Todo-container");
   parentEl.appendChild(newTodo);
