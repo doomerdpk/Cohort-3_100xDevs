75afddcd202e4ac8ddf2f2512c53e77ec21603a2
doomerdpk <doomerdpk@gmail.com>
2025-02-05 03:07:52 +0530

    Improved Code

--- Week_5/QueryParameters/index.js
+++ Week_5/QueryParameters/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/add", function (req, res) {
+function doOperation(req, res, operation) {
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
 
@@ -12,62 +12,37 @@ app.get("/add", function (req, res) {
     return;
   }
 
-  res.status(200).json({
-    message: a + b,
-  });
-});
-
-app.get("/subtract", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
-
-  if (isNaN(a) || isNaN(b)) {
-    res.status(400).json({
-      error: "Please provide valid numbers for the operation.",
-    });
-    return;
-  }
-
-  res.status(200).json({
-    message: a - b,
-  });
-});
-
-app.get("/multiply", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
-
-  if (isNaN(a) || isNaN(b)) {
-    res.status(400).json({
-      error: "Please provide valid numbers for the operation.",
-    });
-    return;
-  }
-
-  res.status(200).json({
-    message: a * b,
-  });
-});
-
-app.get("/divide", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
-
-  if (isNaN(a) || isNaN(b)) {
-    res.status(400).json({
-      eerror: "Please provide valid numbers for the operation.",
-    });
-    return;
-  }
-
-  if (b === 0) {
+  if (operation === "divide" && b === 0) {
     res.status(400).json({ error: "Division by zero is not allowed." });
     return;
   }
 
+  const operations = {
+    add: a + b,
+    subtract: a - b,
+    multiply: a * b,
+    divide: a / b,
+  };
+
   res.status(200).json({
-    message: a / b,
+    message: operations[operation],
   });
+}
+
+app.get("/add", function (req, res) {
+  doOperation(req, res, "add");
+});
+
+app.get("/subtract", function (req, res) {
+  doOperation(req, res, "subtract");
+});
+
+app.get("/multiply", function (req, res) {
+  doOperation(req, res, "multiply");
+});
+
+app.get("/divide", function (req, res) {
+  doOperation(req, res, "divide");
 });
 
 app.listen(3000, function (req, res) {
